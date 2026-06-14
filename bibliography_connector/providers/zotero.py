7934beb75483f4864c037963dacb4087c409fcd0
mhafizazh <mhafiz.azh@gmail.com>
2026-06-13 21:36:19 -0600

    draft

--- bibliography_connector/providers/zotero.py
+++ bibliography_connector/providers/zotero.py
@@ -5,6 +5,7 @@ class ZoteroProvider:
         self.group_id = group_id
         self.collection = collection
         self.items = []
+        self.cleaned_items = []
 
     def _fetch_items(self, collection_key, **kwargs):
         zot = Zotero(library_id=self.group_id, library_type="group")
@@ -15,5 +16,29 @@ class ZoteroProvider:
                 items.extend(self._fetch_items(sub_key, **kwargs))
         return items
 
+    def _semiflatten(self):
+        pass 
+    
+    def _clean_fields(self):
+        pass
+    
+    def _remdup(self):
+        pass
+
+    def _url_consolidate(self):
+
+    def _clean_abstract(self):
+        pass
+    
+    def _add_date_strings(self):
+        pass
+    
+    def _add_author_strings(self):
+        pass
+
+        
     def fetch(self, **kwargs):
         self.items = self._fetch_items(self.collection, **kwargs)
+
+    def transform(self):
+        pass
